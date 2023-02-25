2316bf2cd07339bbbef06b8d4c1c9adb9e9ee4de
sashengpeng <sashengpeng@blued.com>
2023-02-25 18:28:08 +0800

    back to 1

--- test.go
+++ test.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	println(100)
+	println(1)
 }
--- test.php
+++ test.php
@@ -5,5 +5,5 @@ function push(int $a):int{
 }
 
 
-print_r(push(997));
-print_r(push(997));
+print_r(push(1));
+print_r(push(1));
