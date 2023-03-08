22272b8b3b0c6e5cce8ed8fe6b88a8cce12722e8
sashengpeng <sashengpeng@blued.com>
2023-03-08 18:12:21 +0800

    add class

--- test.go
+++ test.go
@@ -1,5 +1,5 @@
 package main
 
 func main() {
-	println(3)
+	println(10)
 }
--- test.php
+++ test.php
@@ -5,7 +5,7 @@ function push(int $a):int{
 }
 
 
-print_r(push(3));
+// print_r(push(3));
 
 function judge(int $v) : bool {
 	if ($v == 1) {
@@ -14,4 +14,18 @@ function judge(int $v) : bool {
 	return false;
 }
 
-function doNothing() : null {}
+function doNothing() : Nothing {
+	return new Nothing;
+}
+
+Class Nothing {
+	public function __set(string $name, $value) {
+		$this->$name = $value;
+	}
+
+	public function __call($name, $arguments) {
+		print_r($arguments);
+	}
+}
+
+doNothing()->call(1,2,3,4,5);
